1733d2b7c5d36a121505b470dd83843445b47c40
ankit.malasiya@gmail.com <Ankit@1989>
2021-08-04 18:45:55 +0200

    deleted other code

--- restful-web-services/src/main/java/com/ankit/controller/CommentsController.java
+++
@@ -1,8 +0,0 @@
-package com.ankit.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController("/comments")
-public class CommentsController {
-
-}
--- restful-web-services/src/main/java/com/ankit/model/Comment.java
+++
@@ -1,7 +0,0 @@
-package com.ankit.model;
-
-//@Entity
-//@Table(name = "COMMENTS")
-public class Comment {
-
-}
